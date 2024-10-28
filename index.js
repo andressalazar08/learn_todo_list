086321e4faf26081fc12372b43ab1230c6e5e6a5
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-10-28 14:22:37 -0500

    database todolist connected

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const app = require('./app');
 require('dotenv').config();
+const  sequelize  = require('./api/config/database');
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+//database autentication
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+});
+
